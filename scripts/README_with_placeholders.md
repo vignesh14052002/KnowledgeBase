0285d2192a5056ba88da1a2984b586e867441092
vignesh14052002 <vignesharivazhagan1409@gmail.com>
2024-07-01 21:49:11 +0530

    add documents count to readme dynamically

--- scripts/README_with_placeholders.md
+++ scripts/README_with_placeholders.md
@@ -17,7 +17,7 @@ The following items will get dynamically updated on every push to `master` branc
 
 | Section          | Count            |
 |------------------|------------------|
-| Documents        | 32 |
+| Documents        | !@(documents_count) |
 | Topics           | !@(topics_count)    |
 | Reference Links  | !@(reference_links) |
 
--- scripts/generate_readme.py
+++ scripts/generate_readme.py
@@ -1,10 +1,37 @@
 import os
+import re
 
-README_PATH = os.path.join(os.path.dirname(__file__), "..", "README.md")
+ROOT_PATH = os.path.join(os.path.dirname(__file__), "..")
+README_WITH_PLACEHOLDERS_PATH = os.path.join(
+    os.path.dirname(__file__), "README_with_placeholders.md"
+)
+README_PATH = os.path.join(ROOT_PATH, "README.md")
 
-with open(README_PATH, "r") as file:
+with open(README_WITH_PLACEHOLDERS_PATH, "r") as file:
     readme = file.read()
+
+
+def get_documents_count():
+    # Count of all markdown files in repo recursively
+    count = 0
+    for root, dirs, files in os.walk(ROOT_PATH):
+        for file in files:
+            if file.endswith(".md"):
+                count += 1
+    return count
+
+
+PLACEHOLDER__FUNCTION_MAP = {"documents_count": get_documents_count}
+PLACEHOLDER_PATTERN = re.compile(r"!\@\((.*?)\)")
+
+# Replace all placeholders with the actual values
+for pattern_match in PLACEHOLDER_PATTERN.finditer(readme):
+    placeholder = pattern_match.group(1)
+    function = PLACEHOLDER__FUNCTION_MAP.get(placeholder)
+    if function:
+        value = function()
+        readme = readme.replace(pattern_match.group(0), str(value))
 
 # Update the README.md file with the latest content
 with open(README_PATH, "w") as file:
-    file.write(readme + "\n" + "This is a new line added by the script.")
+    file.write(readme)
