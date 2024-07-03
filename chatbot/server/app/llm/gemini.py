992a94ce03d1992b6a2abceb361cd6cac3583568
vignesh14052002 <vignesharivazhagan1409@gmail.com>
2024-07-03 20:23:24 +0530

    use dotenv to deploy in render

--- chatbot/server/app/llm/gemini.py
+++ chatbot/server/app/llm/gemini.py
@@ -1,8 +1,10 @@
-from app.config import settings
 from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+
+import app.config
 
 
 def get_gemini_llm(model_name):
     return ChatGoogleGenerativeAI(
-        model=model_name, google_api_key=settings.GEMINI_API_KEY
+        model=model_name, google_api_key=os.getenv("GEMINI_API_KEY")
     )
