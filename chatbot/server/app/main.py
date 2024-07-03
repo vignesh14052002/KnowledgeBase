8af9c36e136e727b6cf10756205344ca5870d4c0
vignesh14052002 <vignesharivazhagan1409@gmail.com>
2024-07-03 16:44:01 +0530

    serve client from backend

--- chatbot/server/app/main.py
+++ chatbot/server/app/main.py
@@ -1,7 +1,11 @@
+import os
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import chat_with_knowledge_base
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(
     title="Chat with Knowledge Base API Documentation",
@@ -23,6 +27,18 @@ app.include_router(
     prefix="/v1/knowledge-base",
     tags=["Chat with Knowledge Base"],
 )
+
+# Serving - static files
+static_files_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static")
+app.mount("/static", StaticFiles(directory=static_files_path), name="static")
+templates = Jinja2Templates(directory=static_files_path)
+
+
+@app.get("/", response_class=HTMLResponse)
+@app.get("/index.html", response_class=HTMLResponse)
+def read_index(request: Request):
+    """Serve the index.html file of front-end code at the root."""
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 def main():
